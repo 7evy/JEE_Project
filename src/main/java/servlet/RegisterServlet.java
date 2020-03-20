8f263f60ca17d64c8936384e2d625009ffc5c8ac
Sebibebi67 <sebibebi67@gmail.com>
2020-03-20 10:13:18 +0100

    register page works

---
+++ src/main/java/servlet/RegisterServlet.java
@@ -0,0 +1,46 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+// import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Describes the servlet used on the register page
+ * @author Sébastien HERT
+ */
+@WebServlet(name = "register", urlPatterns = { "/register" })
+public class RegisterServlet extends HttpServlet {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Displays the page
+     * @param request
+     * @param response
+     * @author Sébastien HERT
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String pageName = "/register.html";
+        RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
+        try {
+            rd.forward(request, response);
+        } catch (ServletException e) {
+            System.out.println("Error : ServletException");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error : IOException");
+            e.printStackTrace();
+        }
+    }
+}
